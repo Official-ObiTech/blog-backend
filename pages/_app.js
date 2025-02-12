de05e6f8ed58ccca58a69627f3a58b81dca24559
Obinna Justice <official.info.obitech@gmail.com>
2025-02-12 17:35:24 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -31,7 +31,7 @@ export default function App({
                 <Component {...pageProps} />
                 <Component {...pageProps} />
                 <Component {...pageProps} />
-                <Component {...pageProps} />
+               
               
                 
               
